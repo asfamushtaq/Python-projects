3d2185f5d1e3bc8cd718f13e8ffe219a9a8a960b
asfamushtaq <asfachhinna@gmail.com>
2026-07-16 02:59:00 -0700

    Add Python Calculator Program
    Added a simple Python calculator program that performs basic arithmetic operations, including addition, subtraction, multiplication, division, floor division, modulus, and exponentiation. This beginner-friendly project demonstrates the use of user input, data type conversion, and arithmetic operators in Python.

---
+++ Calculator.py
@@ -0,0 +1,61 @@
+a=input("Enter first number:")
+b=input("Enter second number:")
+c=int(a)+int(b)
+print("sum of a and b is:",c)
+
+c=int(a)-int(b)
+print("sub of a and b is:",c)
+
+c=int(a)*int(b)
+print("mul of a nad b is:",c)
+
+c=int(a)/int(b)
+print("div of a and b is:",c)
+
+c=int(a)//int(b)
+print("Floor div of a and b is:",c)
+
+c=int(a)%int(b)
+print("mod of a and b is:",c)
+
+c=int(a)**int(b)
+print("exp of a and b is:",c)
+
+                    #....................OR....................
+
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("sum of a and b is:",a+b)
+
+
+print("sub of a and b is:",a-b)
+
+
+print("mul of a nad b is:",a*b)
+
+
+print("div of a and b is:",a/b)
+
+
+print("Floor div of a and b is:",a//b)
+
+
+print("mod of a and b is:",a%b)
+
+
+print("exp of a and b is:",a**b)
+
+
+
+
+
+
+
+
+
+
+
+
+
